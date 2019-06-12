c5a4756052549ca4eeb1ff8c73d221c14cc714ec
MakotoKobayashi0 <b1500578@planet.kanazawa-it.ac.jp>
2019-06-12 15:54:50 +0900

    (2-1)ログイン後のページで{ログイン者名}さんこんにちはと表示されるようにした

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -109,6 +109,7 @@ public class AdministratorController {
 			result.addError(new ObjectError("loginError", "メールアドレスまたはパスワードが不正です。"));
 			return toLogin();
 		}
+		session.setAttribute("administratorName", administrator.getName());
 		return "forward:/employee/showList";
 	}
 	
